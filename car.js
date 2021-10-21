5efddf88524ba0763eb487f8d0e19a923e103a30
KevinJeanMarie <kjeanmarie01@gmail.com>
2021-10-21 16:54:26 +0200

    modif

---
+++ car.js
@@ -0,0 +1,35 @@
+
+class Car {
+    constructor(brand,speed){
+        this.brand = brand
+        this.speed = speed
+       
+    }
+
+    accelerate (vitesse) {
+        this.speed = this.speed + vitesse
+        
+    }
+
+    brake (frein) {
+        this.speed = this.speed - frein 
+    
+    }
+
+    describe (){
+        console.log (`${this.brand} running at ${this.speed}`)
+    }
+
+
+}
+
+let ford= new Car("ford",10)
+console.log(ford)
+
+ford.accelerate(50)
+console.log(ford)
+
+ford.brake(25)
+console.log(ford)
+
+ford.describe()
